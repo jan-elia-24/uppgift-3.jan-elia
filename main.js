176db8ab8fa9c3c66d9876069b4c93677acf4d7b
onormalare24 <jan.elia995@hotmail.com>
2024-09-20 10:16:36 +0200

    adding a comment

--- main.js
+++ main.js
@@ -4,6 +4,8 @@
 const changeh1Tag = document.querySelector("#changeh1Tag");
 const h1Element = document.querySelector("#h1Element");
 
+// add an event to capture the order to change the text in h1
+
 changeh1Tag.addEventListener(
     "click",
     function (event) {
